91e75c3e010c98ea23249431fc15f923c9e845fa
Crowdbotics <team@crowdbotics.com>
2020-10-02 23:03:51 +0000

    Update models

--- backend/dodate_20997/wsgi.py
+++ backend/dodate_20997/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dodate_20997.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dodate_20997.settings")
 
 application = get_wsgi_application()
